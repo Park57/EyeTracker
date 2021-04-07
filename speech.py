272064f96627c4b53afb0aad808e45dd18be3150
Vincent <twix_vr@msn.com>
2021-04-07 16:51:13 +0200

    ajout de speech

---
+++ speech.py
@@ -0,0 +1,16 @@
+#pip3 install gTTS
+from gtts import gTTS
+import os
+
+class Speech: 
+
+    # Constructor
+    def __init__(self):
+        self.language='fr'
+
+    # Read the sentance and create a file .mp3
+    def speakSentence(self,sentence,file):
+        myobj = gTTS(text=sentence, lang=self.language, slow=False)
+        myobj.save("speech/"+file+".mp3")
+        os.system("mpg321 speech/"+file+".mp3")
+        os.remove("speech/"+file+".mp3")
