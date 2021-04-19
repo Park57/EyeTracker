ab5d28e873de917bef86d11014e1badef40e5052
Vincent <twix_vr@msn.com>
2021-04-19 13:01:38 +0200

    Ajout sécurité si le fichier se finit par .txt

--- graph.py
+++ graph.py
@@ -23,46 +23,47 @@ class Graph:
         k = 1
         for element in os.listdir("data/sauvegarde/"+dossier):
             file = "data/sauvegarde/"+dossier+"/"+element
-            with open(file) as f:
-                content = f.readlines()
-            for line in content:
-                coordX = re.match("^\(\d*",line)
-                coordX= coordX.group()
-                lengthX = len(coordX)
-                coordX = coordX[1:lengthX]
+            if(file.endswith('.txt')):
+                with open(file) as f:
+                    content = f.readlines()
+                for line in content:
+                    coordX = re.match("^\(\d*",line)
+                    coordX= coordX.group()
+                    lengthX = len(coordX)
+                    coordX = coordX[1:lengthX]
 
-                coordY = re.search(",\d*",line)
-                coordY = coordY.group()
-                lengthY = len(coordY)
-                coordY = coordY[1:lengthY]
-                dataX = np.append(dataX,int(coordX))
-                dataY = np.append(dataY,int(coordY))
-                print(dataX) #DEBUG
-                print(dataY) #DEBUG
-        
-            #2D array with the coord x,y
-            z = np.column_stack((dataX, dataY))
+                    coordY = re.search(",\d*",line)
+                    coordY = coordY.group()
+                    lengthY = len(coordY)
+                    coordY = coordY[1:lengthY]
+                    dataX = np.append(dataX,int(coordX))
+                    dataY = np.append(dataY,int(coordY))
+                    print(dataX) #DEBUG
+                    print(dataY) #DEBUG
+            
+                #2D array with the coord x,y
+                z = np.column_stack((dataX, dataY))
 
-            plt.figure(1,figsize=(15,8))
-            plt.subplot(3,3,2)
-            plt.scatter(dataX,dataY)
-            plt.title('Nuage de points représentant coordonnées X et Y du regard sur l\'écran')
-            plt.xlabel('x')
-            plt.ylabel('y')
-            i = 1
-            plt.subplot(3,3,8)
-            plt.scatter(dataX,dataY)
+                plt.figure(1,figsize=(15,8))
+                plt.subplot(3,3,2)
+                plt.scatter(dataX,dataY)
+                plt.title('Nuage de points représentant coordonnées X et Y du regard sur l\'écran')
+                plt.xlabel('x')
+                plt.ylabel('y')
+                i = 1
+                plt.subplot(3,3,8)
+                plt.scatter(dataX,dataY)
 
-            plt.title('Nuage de points avec suivi du regard')
-            plt.xlabel('x')
-            plt.ylabel('y')
-            while i < len(dataX):
-                print(i)
-                plt.annotate('', xy=(dataX[i], dataY[i]),xytext=(dataX[i-1],dataY[i-1]),arrowprops=dict(facecolor='black',arrowstyle='->'))
-                i += 1
-            plt.savefig('data/sauvegarde/'+dossier+'/graphics'+str(k)+'.png')
-            k += 1
-            plt.show()
+                plt.title('Nuage de points avec suivi du regard')
+                plt.xlabel('x')
+                plt.ylabel('y')
+                while i < len(dataX):
+                    print(i)
+                    plt.annotate('', xy=(dataX[i], dataY[i]),xytext=(dataX[i-1],dataY[i-1]),arrowprops=dict(facecolor='black',arrowstyle='->'))
+                    i += 1
+                plt.savefig('data/sauvegarde/'+dossier+'/graphics'+str(k)+'.png')
+                k += 1
+                plt.show()
 
 
         
